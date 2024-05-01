451d9c9c56aa797052f0c038f4171fdae0ad4b57
siraj yesuf <sirajyesuf762@gmail.com>
2024-05-01 17:36:17 +0300

    =gallery fixed

--- resources/views/livewire/gallery-page.blade.php
+++ resources/views/livewire/gallery-page.blade.php
@@ -1,12 +1,19 @@
 <div>
 
         <section class="gallery_hero">
-            <p>Explore</p>
-            <img
-                src="{{ asset('img/gallery_hero.jpg') }}"
-                alt="oprifs gallery hero"
-                class="gallery_hero_img"
-            />
+
+            {{-- <div class="gallery_hero__title"> Explore </div> --}}
+
+            <div class="gallery_hero__imgbox">
+                
+                <img
+                    src="{{ asset('img/gallery_background.png') }}"
+                    alt="oprifs gallery hero"
+                    class="gallery_hero_img"
+                />
+
+            </div>
+
         </section>
 
         <section class="gallery">
@@ -25,7 +32,7 @@
 
 
                         <div class="gallery_item__overlay">
-                            {{\Illuminate\Support\Str::limit($gallery->description,50)}}
+                            {{\Illuminate\Support\Str::limit($gallery->description,10)}}
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias placeat dolorum odio aliquam saepe alias voluptates sequi similique reiciendis ut molestiae aspernatur, hic, odit laudantium? Tempora facere debitis iusto odio.
                         </div>
 
