f91336c41e95cf1cc83186194b7707d8d28f0212
Siraj Yesuf <sirajyesuf762@gmail.com>
2025-02-20 14:41:24 +0300

    =filament resourcea for staff created

---
+++ app/Filament/Resources/StaffResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StaffResource\Pages;
+use App\Filament\Resources\StaffResource\RelationManagers;
+use App\Models\Staff;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StaffResource extends Resource
+{
+    protected static ?string $model = Staff::class;
+
+    protected static ?string $navigationIcon = "heroicon-c-user-group";
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make("full_name")
+                ->required()
+                ->maxLength(255),
+            Forms\Components\TextInput::make("position")
+                ->required()
+                ->maxLength(255),
+            Forms\Components\FileUpload::make("picture")
+                ->required()
+                ->image()
+                ->directory("staff")
+                ->image()
+                ->imageEditor()
+                ->columnSpan(2),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make("full_name")->searchable(),
+                Tables\Columns\TextColumn::make("position")->searchable(),
+                Tables\Columns\ImageColumn::make("picture")->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([Tables\Actions\EditAction::make()])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+                //
+            ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            "index" => Pages\ListStaff::route("/"),
+            "create" => Pages\CreateStaff::route("/create"),
+            "edit" => Pages\EditStaff::route("/{record}/edit"),
+        ];
+    }
+}
